c6997744304085fc55b199a94f0a873faca99855
fabellaivan <ivano.fabella@gmail.com>
2023-09-10 20:11:23 -0300

    s
    s

--- src/services/user.service.service.ts
+++ src/services/user.service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword} from '@angular/fire/auth'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private auth: Auth) {}
+
+    register( {email,password} : any){
+      return createUserWithEmailAndPassword(this.auth,email, password);
+   }
 }
